7c5a5fc6d1937a65e9d2293dd7e7a29c54796e72
hypernormalisation <52763122+hypernormalisation@users.noreply.github.com>
2022-09-05 19:39:29 +0100

    Included ranged speed in on-load repoll

--- LibClassicSwingTimerAPI.lua
+++ LibClassicSwingTimerAPI.lua
@@ -71,7 +71,7 @@ function lib:ADDON_LOADED(_, addOnName)
 	self.skipNextAttackSpeedUpdateCount = 0
 
 	-- Repoll the speed a few seconds after init to get around the API
-	-- for UnitAttackSpeed mainhand sometimes returning zero for a short time
+	-- for UnitAttackSpeed mainhand and ranged sometimes returning zero for a short time
 	-- when the game is first loaded.
 	C_Timer.After(3, function()
 		self:InitSwingSpeeds()
@@ -82,6 +82,7 @@ end
 
 function lib:InitSwingSpeeds()
 	local mainSpeed, offSpeed = UnitAttackSpeed("player")
+	self.rangedSpeed = UnitRangedDamage("player") or 0
 	if mainSpeed == 0 then
 		self.mainSpeed = 3.0 -- some dummy non-zero value to prevent infinities
 	else
@@ -254,7 +255,6 @@ function lib:COMBAT_LOG_EVENT_UNFILTERED(_, ts, subEvent, _, sourceGUID, _, _, _
 end
 
 function lib:UNIT_ATTACK_SPEED()
-	print('UNIT_ATTACK_SPEED call made')
 	local now = GetTime()
 	if
 		self.skipNextAttackSpeedUpdate
